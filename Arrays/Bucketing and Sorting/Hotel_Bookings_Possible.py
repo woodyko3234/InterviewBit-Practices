c13dd6c97201ffd3d2d1cd75c2b702bcb9eb3156
Woody Yao <bogikidd@gmail.com>
2018-12-30 23:52:37 +0200

    Create Hotel_Bookings_Possible.py

---
+++ Arrays/Bucketing and Sorting/Hotel_Bookings_Possible.py
@@ -0,0 +1,21 @@
+class Solution:
+    # @param arrive : list of integers
+    # @param depart : list of integers
+    # @param K : integer
+    # @return a boolean
+    def hotel(self, arrive, depart, K):
+        events = [(t, 1) for t in arrive] + [(t, 0) for t in depart]
+        events = sorted(events)
+
+        guests = 0
+
+        for event in events:
+            if event[1] == 1:
+                guests += 1
+            else:
+                guests -= 1
+
+            if guests > K:
+                return 0
+
+        return 1

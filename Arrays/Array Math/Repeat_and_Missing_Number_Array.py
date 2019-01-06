bc2a9c4a067ab3546e73decb403173452906ff23
Woody Yao <bogikidd@gmail.com>
2019-01-06 14:07:57 +0200

    Update Repeat_and_Missing_Number_Array.py
    update the solution without applying additional memory complexity

--- Arrays/Array Math/Repeat_and_Missing_Number_Array.py
+++ Arrays/Array Math/Repeat_and_Missing_Number_Array.py
@@ -1,3 +1,32 @@
+#Without applying additional memory
+class Solution:
+    # @param A : tuple of integers
+    # @return a list of integers
+    def repeatedNumber(self, A):
+        
+        n = len(A)
+        
+        sum_real =sum(A) 
+        sum_square_real= 0
+        
+        for x in A:
+            sum_square_real +=  x*x
+            
+        A_B = sum_real - (n*(n+1))//2
+        
+        
+        A2_B2 = sum_square_real - (n*(n+1)*(2*n+1))//6
+        
+        A_plus_B = A2_B2/A_B
+        
+        missing = (abs(A_plus_B+A_B))/2
+        
+        repeat = (abs(A_plus_B - A_B))/2
+        
+        return [int(missing),int(repeat)]
+
+'''
+#Easy way to solve this problem but apply n memory complexity
 from collections import Counter
 class Solution:
     # @param A : tuple of integers
@@ -15,3 +44,4 @@ class Solution:
             else:
                 output[1] = i
         return output
+'''

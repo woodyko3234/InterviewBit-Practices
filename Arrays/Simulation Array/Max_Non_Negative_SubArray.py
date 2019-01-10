412d8f3f253b6c0d86578a60c1a3c8ba0d6eb7c5
Woody Yao <bogikidd@gmail.com>
2019-01-10 22:41:37 +0200

    simplify the code
    make the code shorter and easier to understand

--- Arrays/Simulation Array/Max_Non_Negative_SubArray.py
+++ Arrays/Simulation Array/Max_Non_Negative_SubArray.py
@@ -2,32 +2,21 @@ class Solution:
     # @param A : list of integers
     # @return a list of integers
     def maxset(self, A):
-        if len(A) == 0: return []
-        start_pos = 0
-        maximum = 0
-        max_list = []
-        last_neg_pos = -1
-        current_list = []
-        current_max = 0
-        for idx, num in enumerate(A):
-            if num >= 0 and idx - last_neg_pos == 1:
-                start_pos = idx
-                current_max += num
-                current_list.append(num)
-            elif num >= 0:
-                current_max += num
-                current_list.append(num)
+        n = len(A)
+        if n == 0: return []
+        maximum, current_max =0, 0
+        max_list, current_list = [], []
+        for i in range(n):
+            if A[i] >= 0: 
+                current_list.append(A[i])
             else:
-                last_neg_pos = idx
-                if maximum < current_max:
+                current_max = sum(current_list)
+                if current_max > maximum or (current_max == maximum and len(current_list)>len(max_list)):
                     maximum = current_max
                     max_list = current_list
-                elif maximum == current_max and len(max_list) < len(current_list):
-                    max_list = current_list
-                current_max = 0
                 current_list = []
-        if maximum < current_max:
-            max_list = current_list
-        elif maximum == current_max and len(max_list) < len(current_list):
+        current_max = sum(current_list)
+        if current_max > maximum or (current_max == maximum and len(current_list)>len(max_list)):
+            maximum = current_max
             max_list = current_list
         return max_list

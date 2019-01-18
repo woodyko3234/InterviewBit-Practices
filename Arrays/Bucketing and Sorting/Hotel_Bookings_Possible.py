e63ab43f72bdf8fe06a8ed5f6da5c958956c39eb
Woody Yao <bogikidd@gmail.com>
2019-01-18 13:37:43 +0200

    Update Hotel_Bookings_Possible.py
    with while loop solution

--- Arrays/Bucketing and Sorting/Hotel_Bookings_Possible.py
+++ Arrays/Bucketing and Sorting/Hotel_Bookings_Possible.py
@@ -4,6 +4,32 @@ class Solution:
     # @param K : integer
     # @return a boolean
     def hotel(self, arrive, depart, K):
+        occupancy = 0
+        n = len(arrive)
+        if n == 1:
+            if K > 0: return True
+            else: return False
+        elif n == 0: return True
+        arrive = sorted(arrive)
+        depart = sorted(depart)
+       
+        i, j = 0, 0
+        while i < n and j < n:
+            if arrive[i] < depart[j]:
+                occupancy += 1
+                if occupancy > K: return False
+                i += 1
+            else:
+                occupancy -= 1
+                j += 1
+        return True
+'''
+class Solution:
+    # @param arrive : list of integers
+    # @param depart : list of integers
+    # @param K : integer
+    # @return a boolean
+    def hotel(self, arrive, depart, K):
         events = [(t, 1) for t in arrive] + [(t, 0) for t in depart]
         events = sorted(events)
 
@@ -19,3 +45,4 @@ class Solution:
                 return 0
 
         return 1
+'''

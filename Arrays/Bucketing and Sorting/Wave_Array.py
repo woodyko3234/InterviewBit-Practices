30429b8018ac9aebf6372d7085368d19d1e8ee89
Woody Yao <bogikidd@gmail.com>
2019-01-18 14:12:58 +0200

    Update Wave_Array.py
    simplify the solution

--- Arrays/Bucketing and Sorting/Wave_Array.py
+++ Arrays/Bucketing and Sorting/Wave_Array.py
@@ -2,38 +2,12 @@ class Solution:
     # @param A : list of integers
     # @return a list of integers
     def wave(self, A):
-        if len(A) == 0: return []
-        elif len(A) == 1: return A
-        result = []
-        a_len = len(A)
+        # For the answer to be the lexicographically smallest,
+        # we want the first element to be the second smallest,
+        # the 2nd element to be the smallest, and so on with
+        # the rest of the sequence.
         A = sorted(A)
-        for i in range(1, a_len-1):
-            if i % 2 == 1 and A[i-1] >= A[i]:
-                result.append(A[i-1])
-            elif i % 2 == 1 and A[i-1] < A[i]:
-                A[i], A[i-1] = A[i-1], A[i]
-                result.append(A[i-1])
-            elif i % 2 == 0 and A[i-1] <= A[i]:
-                result.append(A[i-1])
-            elif i % 2 == 0 and A[i-1] > A[i]:
-                A[i], A[i-1] = A[i-1], A[i]
-                result.append(A[i-1])
-        current = A[a_len - 2]
-        nextt = A[a_len - 1]
-        if (a_len - 2) % 2 == 1 and current <= nextt:
-            result.append(current)
-            result.append(nextt)
-        elif (a_len - 2) % 2 == 1 and current > nextt:
-            current, nextt = nextt, current
-            result.append(current)
-            result.append(nextt)
-        if (a_len - 2) % 2 == 0 and current >= nextt:
-            result.append(current)
-            result.append(nextt)
-        elif (a_len - 2) % 2 == 0 and current < nextt:
-            current, nextt = nextt, current
-            result.append(current)
-            result.append(nextt)    
-
-        return result
+        for i in range(0, len(A)-1, 2):
+            A[i], A[i+1] = A[i+1], A[i]
+        return A
                 

6d5c224dc5858f7144854628c83408c03f337e1f
Woody Yao <bogikidd@gmail.com>
2019-02-02 00:34:54 +0200

    Update Maximum_Consecutive_Gap.py
    with true solution in linear complexity

--- Arrays/Bucketing and Sorting/Maximum_Consecutive_Gap.py
+++ Arrays/Bucketing and Sorting/Maximum_Consecutive_Gap.py
@@ -1,11 +1,31 @@
 class Solution:
-    # @param A : tuple of integers
-    # @return an integer
-    def maximumGap(self, A):
-        if len(A) < 2: return 0
-        #brute force method
-        a_sort = sorted(list(A))
-        max_diff = 0
-        for i in range(len(a_sort)-1):
-            max_diff = max(max_diff, a_sort[i+1]-a_sort[i])
-        return max_diff
+    def maximumGap(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: int
+        """
+        if len(arr) < 2:
+            return 0
+
+        min_v, max_v = min(arr), max(arr)
+        if max_v - min_v < 2:
+            return max_v - min_v
+
+        min_gap = max(1, (max_v - min_v) // (len(arr)-1))  # The minimum possible gap
+        sentinel_min, sentinel_max = 2 ** 32 - 1, -1
+        buckets_min = [sentinel_min] * len(arr)
+        buckets_max = [sentinel_max] * len(arr)
+
+        for x in arr:
+            i = min((x - min_v) // min_gap, len(arr) - 1)
+            buckets_min[i] = min(buckets_min[i], x)
+            buckets_max[i] = max(buckets_max[i], x)
+        max_gap = 0
+        prev_max = buckets_max[0]  # First gap is always non-empty
+        for i in range(1, len(arr)):
+            if buckets_min[i] == sentinel_min:
+                continue
+            max_gap = max(buckets_min[i] - prev_max, max_gap)
+            prev_max = buckets_max[i]
+
+        return max_gap

a53cc58068cac6c56e96c0b59b65bb888f38a094
Woody Yao <bogikidd@gmail.com>
2019-02-12 13:44:53 +0200

    Update Sum_of_pairwise_Hamming_Distance.py
    We don't need the math module to complete this; Set an if statement to break the loop so we don't need to look up to (2**32)-1 in every case.

--- Math/Adhoc/Sum_of_pairwise_Hamming_Distance.py
+++ Math/Adhoc/Sum_of_pairwise_Hamming_Distance.py
@@ -1,11 +1,10 @@
-import math
-
 class Solution:
     # @param A : tuple of integers
     # @return an integer
     def hammingDistance(self, A):
         n = len(A)
         c=0
+        m = max(A)
         for i in range(32):
             mask = 1<<i
             t = 0
@@ -13,4 +12,6 @@ class Solution:
                 if(mask&a):
                     t += 1
             c += (t*(n-t))
+            #Set an if statement to break the loop so we don't need to look up to (2**32)-1 in every case.
+            if m <= mask: break
         return (2*c)%1000000007

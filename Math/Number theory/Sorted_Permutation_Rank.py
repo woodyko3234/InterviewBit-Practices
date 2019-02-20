c853e6c5b0ad76deedeedc41ed0646799bf0371d
Woody Yao <bogikidd@gmail.com>
2019-02-20 23:23:36 +0200

    Update Sorted_Permutation_Rank.py

--- Math/Number theory/Sorted_Permutation_Rank.py
+++ Math/Number theory/Sorted_Permutation_Rank.py
@@ -4,11 +4,16 @@ class Solution:
     # @return an integer
     def findRank(self, A):
         rank = 1
-        letters = sorted(list(A))
+        n = len(A)
+        letter_ord = [ord(i) for i in A]
+        #print(letter_ord)
         for i, c in enumerate(A):
-            pos = letters.index(c)
-            letters.remove(c)
-            rank += pos * factorial(len(letters))
+            cur = letter_ord[i]
+            cur_rank = 0
+            for j in range(i+1, n):
+                if letter_ord[i] > letter_ord[j]: cur_rank +=1
+            #print(cur)
+            rank += cur_rank * factorial(n-i-1)
             if rank > 1000003:
-                rank = rank % 1000003
+                rank = rank % 1000003    
         return rank

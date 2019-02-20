90f5436f6a5829e5e860ee5065c7b5be36dbbfb1
Woody Yao <bogikidd@gmail.com>
2019-02-20 22:00:08 +0200

    Update Trailing_Zeros_in_Factorial.py
    to make it easier

--- Math/Number theory/Trailing_Zeros_in_Factorial.py
+++ Math/Number theory/Trailing_Zeros_in_Factorial.py
@@ -2,19 +2,10 @@ class Solution:
     # @param A : integer
     # @return an integer
     def trailingZeroes(self, A):
-        #only need to count how many 5s
         if A < 5: return 0
-
-        fives = 0
-        for i in range(5, A+1, 5):
-            fives += 1
-        
-        output = 0
-        times = 1
-        if fives < 5: return fives
-        else:
-            while fives > 0:
-                output += fives
-                fives  = fives // 5
-                times += 1
-            return output
+        power = 1
+        zeros = 0
+        while pow(5, power) <= A:
+            zeros += A//pow(5, power)
+            power += 1
+        return zeros

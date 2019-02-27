5cf019d169f3ca2f1bd2937afcb1143c2c7789fc
Woody Yao <bogikidd@gmail.com>
2019-02-27 22:42:46 +0200

    Update Numbers_of_length_N_and_value_less_than_K.py
    with more clear and decent codes

--- Math/Array dp/Numbers_of_length_N_and_value_less_than_K.py
+++ Math/Array dp/Numbers_of_length_N_and_value_less_than_K.py
@@ -3,40 +3,26 @@ class Solution:
     # @param B : integer
     # @param C : integer
     # @return an integer
-    def get_count(self, A, B, base, index):
-        if index >= len(base) or B==0:
-            return 1
-        current_max = int(base[index])
-        rt = 0
-        for x in A:
-            if index == 0 and x ==0 and B!=1:
-                continue
-            if x > current_max:
-                continue
-            if x == current_max:
-                if index >= len(base) -1:
-                    continue
-                rt = rt + self.get_count(A, B-1, base, index+1)
-            else:
-                rt = rt + len(A)**(B-1)
-        return rt
-                
-    
     def solve(self, A, B, C):
-        base = str(C)
-        if len(base) < B:
-            return 0
-        
-        index = 0
-        if len(base) == B:
-            return self.get_count(A, B, base, 0)
-        rt = 0
-        for x in A:
-            if x == 0 and B!=1:
-                continue
-            if x==0 and x < C:
-                rt +=1
-            else:
-                rt = rt + len(A)**(B-1)
-        return rt
-            
+        if not A: return 0
+        elif C < 10**(B-1): return 0
+        elif C > 10**B: 
+            n = len(A)
+            if A[0] == 0 and B > 1: return (n-1) * pow(n, B-1)
+            else: return pow(n, B)
+        elif B == 1: return len([i for i in A if i < C])
+        else:
+            n = len(A)
+            T = str(C)
+            output = 0
+            for i in range(B):
+                temp_lt = 0
+                temp_eq = 0
+                for j in A:
+                    if j < int(T[i]): temp_lt += 1
+                    elif j == int(T[i]): temp_eq = 1
+                    else: break
+                if i == 0 and A[0] == 0: output += (temp_lt-1) * pow(n, B-i-1)
+                else: output += temp_lt * pow(n, B-i-1)
+                if temp_eq == 0: break
+        return output

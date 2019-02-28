f1108691b53bce0a0afef78e3c5dc83daff344e1
Woody Yao <bogikidd@gmail.com>
2019-03-01 00:10:45 +0200

    Update Grid_Unique_Paths.py

--- Math/Combinatorics/Grid_Unique_Paths.py
+++ Math/Combinatorics/Grid_Unique_Paths.py
@@ -1,13 +1,13 @@
+import math
 class Solution:
     # @param A : integer
     # @param B : integer
     # @return an integer
     def uniquePaths(self, A, B):
-        A -=1;
-        B-=1;
-        num = A+B
-        den = A
-        res = 1
-        for i in range(A):
-            res *= (num-i)/(den-i)
-        return int(round(res))
+        steps_right = A - 1
+        steps_down = B - 1
+        fr = math.factorial(steps_right)
+        fd = math.factorial(steps_down)
+        ft = math.factorial(steps_right + steps_down)
+        # print(fr, fd, (fr + fd), (fr * fd))
+        return ft // (fr * fd)

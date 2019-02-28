38a01693a509d6cce450efc0d676d1f50619c241
Woody Yao <bogikidd@gmail.com>
2019-02-28 22:24:07 +0200

    Update Rearrange_Array.py
    deepcopy is unacceptable according to the question; change with appropriate codes

--- Math/Number encoding/Rearrange_Array.py
+++ Math/Number encoding/Rearrange_Array.py
@@ -1,18 +1,11 @@
-import copy
 class Solution:
     # @param A : list of integers
     # Modify the array A which is passed by reference. 
     # You do not need to return anything in this case. 
     def arrange(self, A):
-        #All elements in the array are in the range [0, N-1]
-        a_len = len(A)
-        if a_len <= 1: return A
-        
-        B = copy.deepcopy(A)
-        
-        for i in range(a_len):
-            new_idx = A[i]
-            A[i] = B[new_idx]
-            
-        return A
+        n = len(A)
+        for i in range(n):
+            A[i] += (A[A[i]]%n)*n
+        for i in range(n):
+            A[i] = A[i]//n
             

f8e149324ad864cb5ed9e8d59717c4ab00069da4
Woody Yao <bogikidd@gmail.com>
2019-03-21 11:10:15 +0200

    Update Square_Root_of_Integer.py

--- Binary Search/Search Answer/Square_Root_of_Integer.py
+++ Binary Search/Search Answer/Square_Root_of_Integer.py
@@ -3,11 +3,11 @@ class Solution:
     # @return an integer
     def sqrt(self, A):
         low = 1
-        high = int(A/2)
-        if A == 0 or A == 1: return A
+        high = A//2
+        if A <= 1: return A
         
         while low <= high:
-            mid = int((low + high)/2)
+            mid = (low + high)//2
             midsqr = mid * mid
             if midsqr < A:
                 low = mid + 1
@@ -15,4 +15,4 @@ class Solution:
                 high = mid - 1
             else: return mid
             
-        return int((low + high)/2)
+        return (low + high)//2

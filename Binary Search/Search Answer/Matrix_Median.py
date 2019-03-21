6a35ed7bc8df75077195b49332fdb4f920e7404b
Woody Yao <bogikidd@gmail.com>
2019-03-21 09:04:19 +0200

    Update Matrix_Median.py

--- Binary Search/Search Answer/Matrix_Median.py
+++ Binary Search/Search Answer/Matrix_Median.py
@@ -2,14 +2,26 @@ class Solution:
     # @param A : list of list of integers
     # @return an integer
     def findMedian(self, A):
-        B=[]
-        for i in range(0,len(A)):
-            for j in A[i]:
-                B.append(j)
-        B.sort()
-        #print(B)
-        if len(B)%2==0:
-            median=(B[len(B)//2]+B[len(B)//2-1])//2
+        min_a,max_a,n,m = A[0][0],A[0][0],len(A),len(A[0])
+        for i in range(n):
+            min_a = A[i][0] if A[i][0] < min_a else min_a
+            max_a = A[i][-1] if A[i][-1] > max_a else max_a
+        elements = (n*m+1)//2
+        while min_a < max_a :
+            a_mid,count = (min_a+max_a)//2,0
+            for row in A:
+                count += self.bisectRight(row,a_mid, 0, m)
+            if count < elements:
+                min_a = a_mid+1
+            else:
+                max_a = a_mid
+        return min_a
+
+    def bisectRight(self, lst, a_mid, start, end):
+        if start == end:
+            return end
+        lst_counts = (start+end)//2
+        if lst[lst_counts] <= a_mid:
+            return self.bisectRight(lst, a_mid, lst_counts+1, end)
         else:
-            median=B[(len(B)-1)//2]
-        return median
+            return self.bisectRight(lst, a_mid, start, lst_counts)

7f66fc4362794ef8db7e69084b22793b6b10ab22
Woody Yao <bogikidd@gmail.com>
2019-04-03 22:39:17 +0200

    Update Implement_Power_Function.py

--- Binary Search/Search Step Simulation/Implement_Power_Function.py
+++ Binary Search/Search Step Simulation/Implement_Power_Function.py
@@ -4,26 +4,21 @@ class Solution:
     # @param d : integer
     # @return an integer
     def pow(self, x, n, d):
-        #return (x ** n) % d
-        if x == 0: return x
+        if d == 1: return 0
+        elif x == 0: return x
         elif n == 0: return 1
-        elif d == 1: return 0
-        neg_check = 0
-        if x < 0:
-            x = abs(x)
-            if n % 2 != 0:
-                neg_check = 1
+        elif n == 1: return x % d
+        r = x % d
+        oddP=[]
+        while n >= 2:
+            if n % 2 == 1:
+                oddP.append(r)
+            r = (r**2) % d
+            n //= 2
+        for i in oddP:
+            r = (r * i) % d
+        return r 
     
-        r = 1
-        while n > 0:
-            if n & 1 == 1:
-                r = r * x % d
-            n = int(n/2)
-            x = x * x % d
-        
-        if neg_check == 1:
-            return d - r
-        return r
     #Editorial anwser
     #def pow(self, x, n, d):
         

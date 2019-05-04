2e15d638f125e4e68089f4787ed109c5135c31a5
Woody Yao <bogikidd@gmail.com>
2019-05-04 13:32:17 +0200

    Update Palindrome_String.py

--- Strings/String simulation/Palindrome_String.py
+++ Strings/String simulation/Palindrome_String.py
@@ -2,24 +2,18 @@ class Solution:
     # @param A : string
     # @return an integer
     def isPalindrome(self, A):
-        a_lower = A.lower()
-        a_list = list(a_lower)
-        a_len = len(a_list)
-        for i in range(a_len):
-            if  (97 <= ord(a_list[i]) <= 122) or (48 <= ord(a_list[i]) <= 57): 
-                continue
-            else: 
-                a_list[i] = ''
-        a_lower = ''.join(a_list) #update content
-        a_len = len(a_lower) #update length
+        for c in set(A):
+            if c.isalnum(): continue
+            else: A = A.replace(c, " ")
+        test_str = "".join(A.split(" ")).lower()
         
-        left = 0
-        right = a_len -1
-        while left <= right:
-            if a_lower[left] != a_lower[right]:
+        l = len(test_str)
+        if l % 2 == 0:
+            mid = l // 2 - 1
+        else:
+            mid = l // 2
+        #test
+        for i in range(mid + 1):
+            if test_str[i] != test_str[l-1 - i]:
                 return 0
-            else: 
-                left += 1
-                right -= 1
-        
         return 1

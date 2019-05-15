9691504429da62585f99ac1e64eac56717b00cdc
Woody Yao <bogikidd@gmail.com>
2019-05-15 13:30:32 +0200

    Update Compare_Version_Numbers.py
    make the code simpler

--- Strings/String parsing/Compare_Version_Numbers.py
+++ Strings/String parsing/Compare_Version_Numbers.py
@@ -3,22 +3,17 @@ class Solution:
     # @param B : string
     # @return an integer
     def compareVersion(self, A, B):
-        a_split = A.split('.')
-        b_split = B.split('.')
-        min_len = min(len(a_split), len(b_split))
-            
-        for i in range(min_len):
-            if int(a_split[i]) > int(b_split[i]): return 1
-            elif int(a_split[i]) < int(b_split[i]): return -1
-        
-        if len(a_split) > len(b_split):
-            for j in range(min_len, len(a_split)):
-                if int(a_split[j]) > 0:
-                    return 1
-            return 0
-        elif len(a_split) < len(b_split): 
-            for j in range(min_len, len(b_split)):
-                if int(b_split[j]) > 0:
-                    return -1
-            return 0
-        else: return 0
+        splitA = list(map(int, A.split(".")))
+        splitB = list(map(int, B.split(".")))
+        i, j = len(splitA), len(splitB)
+        min_len = min(i, j)
+        for c in range(min_len):
+            if splitA[c] > splitB[c]: return 1
+            elif splitA[c] < splitB[c]: return -1
+        if i > min_len: 
+            for c in range(min_len, i):
+                if splitA[c] > 0: return 1
+        elif j > min_len: 
+            for c in range(min_len, j):
+                if splitB[c] > 0: return -1
+        return 0

93c56cf902afc6dfba1bd7b830b09cc741b3cc56
Woody Yao <bogikidd@gmail.com>
2019-05-22 07:30:43 +0200

    Update Atoi.py
    remove unnecessary lines of codes and make it more structured

--- Strings/String parsing/Atoi.py
+++ Strings/String parsing/Atoi.py
@@ -2,31 +2,22 @@ class Solution:
     # @param A : string
     # @return an integer
     def atoi(self, A):
-        start = 0
-        if len(A) == 0 : return 0
-        elif (A[start] != '-' and A[start] != '+') and (not A[start].isdigit()): 
-            return 0
-        else:
-            max_atoi = A[0]
-            for i in range(1, len(A)):
-                if A[i].isdigit():
-                    max_atoi += A[i]
-                else:
-                    try:
-                        if int(max_atoi) >= 2147483648:
-                            return 2147483647
-                        elif int(max_atoi) <= -2147483648:
-                            return -2147483648
-                        else:
-                            return int(max_atoi)
-                    except:
-                        return 0
-            try:
-                if int(max_atoi) >= 2147483648:
-                    return 2147483647
-                elif int(max_atoi) <= -2147483648:
-                    return -2147483648
-                else:
-                    return int(max_atoi)
-            except:
-                return 0
+        A_split = A.split()
+        for i in A_split:
+            if not i: continue
+            output = 0
+            minus = False
+            if i[0] == "-": minus = True
+            for j in range(len(i)):
+                if j == 0 and (i[j] == "-" or i[j] == "+"): continue
+                if i[j].isnumeric():
+                    if minus:
+                        output = output * 10 - int(i[j])
+                        if output < (-1 * (2**31)): return -1 * (2**31)
+                    else:
+                        output = output * 10 + int(i[j])
+                        if output > (2**31 - 1): return 2**31 - 1
+                else: return output
+            if output != 0: return output
+            else: return 0
+        return 0    

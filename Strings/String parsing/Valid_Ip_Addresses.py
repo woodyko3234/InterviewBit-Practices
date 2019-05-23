f2d1137f04a799c7844687c4fa114207109a189e
Woody Yao <bogikidd@gmail.com>
2019-05-23 16:49:46 +0200

    Update Valid_Ip_Addresses.py

--- Strings/String parsing/Valid_Ip_Addresses.py
+++ Strings/String parsing/Valid_Ip_Addresses.py
@@ -14,17 +14,17 @@ class Solution:
         #range from 4 to 12
         else:
             for i in range(1, l - 2):
-                if i > 3: break #do not generate irrelevent combinations
+                if i > 3: break #do not generate irrelevant combinations
                 elif l - i > 9: continue #avoid generating items including more than 3 char
                 elif A[0] == '0' and i > 1: break #remove "0??" conditions
                 for j in range(i+1, l - 1):
                     if A[i] == '0' and j > i+1: break #remove "0??" conditions
-                    elif j - i > 3: break #do not generate irrelevent combinations
+                    elif j - i > 3: break #do not generate irrelevant combinations
                     elif l - j > 6: continue #avoid generating items including more than 3 char
                     for k in range(j+1, l):
                         if A[j] == '0' and k > j+1: break #remove "0??" conditions
                         elif A[k] == 0 and (l - k > 1): continue #remove "0??" conditions
-                        elif k - j > 3: break #do not generate irrelevent combinations
+                        elif k - j > 3: break #do not generate irrelevant combinations
                         elif l - k > 3: continue #avoid generating items including more than 3 char
                         ip = A[:i] + '.'
                         ip += A[i:j] + '.'

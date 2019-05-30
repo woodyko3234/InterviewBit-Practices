05508464c7f8d2d25e7933d39243333ea8524f77
Woody Yao <bogikidd@gmail.com>
2019-05-30 22:12:32 +0200

    Update Add_Binary_Strings.py

--- Strings/String math/Add_Binary_Strings.py
+++ Strings/String math/Add_Binary_Strings.py
@@ -1,23 +1,21 @@
 class Solution:
-    # @param A : string
-    # @param B : string
-    # @return a strings
-    def addBinary(self, A, B):
-        AplusB = list(str(int(A) + int(B)))
-        length = len(AplusB)
-        
-        idx = length - 1
-        while idx >= 0:
-            if int(AplusB[idx]) // 2 == 1:
-                AplusB[idx] = '%d' % (int(AplusB[idx]) - 2)
-                if idx - 1 >= 0:
-                    if AplusB[idx-1] == '1':
-                        AplusB[idx-1] = '2'
-                    elif AplusB[idx-1] == '2':
-                        AplusB[idx-1] = '3'
-                    else:
-                        AplusB[idx-1] = '1'
-                else:
-                    AplusB = ['1'] + AplusB
-            idx -= 1
-        return "".join(AplusB)
+	# @param A : string
+	# @param B : string
+	# @return a strings
+	def addBinary(self, A, B):
+	    bin_A, bin_B = int(A), int(B)
+	    com_bin = bin_A + bin_B
+	    update_bin = ""
+	    plus_one = False
+	    for i in str(com_bin)[::-1]:
+	        if i == "2" and plus_one:
+	            update_bin = "1" + update_bin
+	        elif i == "2" or (i == "1" and plus_one):
+	            update_bin = "0" + update_bin
+	            plus_one = True
+	        else:
+	            update_bin = str(int(i) + plus_one) + update_bin
+	            plus_one = False
+	    if plus_one:
+	        update_bin = "1" + update_bin
+	    return update_bin

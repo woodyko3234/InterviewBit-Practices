9f805ca386182f7a324d7c9627045cc9da2caba8
Woody Yao <bogikidd@gmail.com>
2019-06-05 23:16:04 +0200

    Update Justified_Text.py

--- Strings/Pretty print/Justified_Text.py
+++ Strings/Pretty print/Justified_Text.py
@@ -1,56 +1,51 @@
 class Solution:
-    # @param A : list of strings
-    # @param B : integer
-    # @return a list of strings
-    def fullJustify(self, A, B):
-        w_count = len(A)
-        if w_count == 0: return []
-        w_combine = []
-        limits = 0
-        idx = 0
-        sentence = ""
+	# @param A : list of strings
+	# @param B : integer
+	# @return a list of strings
+	def fullJustify(self, A, B):
+	    if not A: return A
+	    length_A = [len(i) for i in A]
+	    sentences = []
+	    temp, temp_l = [], 0
+	    for i, l in zip(A, length_A):
+	        #each word is guaranteed not to exceed B.
+	        if temp == []:
+	            temp.append(i)
+	            temp_l += l
+	        elif (temp_l + 1 + l) <= B:
+	            temp.append(i)
+	            temp_l += 1 + l
+	        else:
+	            #check how many blanks we need
+	            sentence = self.blankAllocate(temp, temp_l, B)
+	            sentences.append(sentence)
+	            temp, temp_l = [], 0
+	            temp.append(i)
+	            temp_l += l
+	    sentence = " ".join(temp)
+	    sentence += " " * (B - len(sentence))
+        sentences.append(sentence)
+	    return sentences
     
-        while idx < w_count:
-            if limits == 0:
-                sentence += A[idx]
-                limits += len(A[idx])
-                idx += 1
-            elif limits + 1 + len(A[idx]) <= B:
-                sentence = sentence + ' ' + A[idx]
-                limits += (1+len(A[idx]))
-                idx += 1
+    def blankAllocate(self, temp, temp_l, B):
+        blank_intervals = len(temp) - 1
+        total_blanks = B - (temp_l - blank_intervals)
+        if blank_intervals > 0:
+            base_blanks = total_blanks // blank_intervals
+            if base_blanks * blank_intervals != total_blanks:
+                adding_blanks = total_blanks % blank_intervals
+            else: adding_blanks = 0
+        else: 
+            base_blanks = B - temp_l
+            adding_blanks = 0
+        sentence = ""
+        if len(temp) == 1: return temp[0] + " " * base_blanks
+        for w in temp:
+            if sentence == "":
+                sentence += w
+            elif adding_blanks > 0:
+                sentence += (" " * (base_blanks + 1)) + w
+                adding_blanks -= 1
             else:
-                if limits == B: pass
-            
-                else:
-                    section = sentence.split(' ')
-                    sen_len = sum(len(i) for i in section)
-                    if len(section) == 1:
-                        spaces = ' ' * (B - sen_len)
-                        sentence = section[0] + spaces
-                    elif (B - sen_len) % (len(section) - 1) == 0:
-                        spaces = ' ' * ((B - sen_len) / (len(section) - 1))
-                        sentence = spaces.join(section)
-                    else:
-                        left_times = (B - sen_len) % (len(section) - 1)
-                        left_spaces = ' ' * (((B - sen_len) / (len(section) - 1)) + 1)
-                        right_times = (len(section) - 1) - left_times
-                        right_spaces = ' ' * ((B - sen_len) / (len(section) - 1))
-                        sentence = ''
-                        for i in range(len(section)):
-                            sentence += section[i]
-                            if left_times > 0:
-                                sentence += left_spaces
-                                left_times -= 1
-                            elif right_times > 0:
-                                sentence += right_spaces
-                                right_times -= 1
-                            else: pass
-                w_combine.append(sentence)
-                sentence = ""
-                limits = 0
-        if limits < B:
-            sentence += ' ' * (B - limits)
-        w_combine.append(sentence)
-        return w_combine
-                        
+                sentence += (" " * (base_blanks)) + w
+        return sentence

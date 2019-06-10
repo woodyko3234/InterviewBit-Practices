d698b04eeacac374a0145ef7b8c374254e38a613
Woody Yao <bogikidd@gmail.com>
2019-06-10 16:45:32 +0200

    Update Number_of_1_Bits.py
    reducing code lines

--- Bit Manipulation/Bit play/Number_of_1_Bits.py
+++ Bit Manipulation/Bit play/Number_of_1_Bits.py
@@ -2,5 +2,4 @@ class Solution:
     # @param A : integer
     # @return an integer
     def numSetBits(self, A):
-        bitOnes = "".join(bin(A)[2:].split('0'))
-        return len(bitOnes)
+        return len("".join(bin(A)[2:].split("0")))

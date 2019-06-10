a60105796610e980e72a7b8fa0557a3b2190c2ae
Woody Yao <bogikidd@gmail.com>
2019-06-11 01:43:20 +0200

    Update Reverse_Bits.py

--- Bit Manipulation/Bit play/Reverse_Bits.py
+++ Bit Manipulation/Bit play/Reverse_Bits.py
@@ -2,10 +2,8 @@ class Solution:
     # @param A : unsigned integer
     # @return an unsigned integer
     def reverse(self, A):
-        reverse = [0] * 32
         reversedBin = bin(A)[:1:-1]
         output = 0
         for i in range(len(reversedBin)):
-            reverse[i] = int(reversedBin[i])
-            output += reverse[i] * (2**(31 - i))
+            output += int(reversedBin[i]) * (2**(31 - i))
         return output

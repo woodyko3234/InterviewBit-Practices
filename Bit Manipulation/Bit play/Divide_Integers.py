eee9bb67553ec11b1ae45d02602d655a81e512fc
Woody Yao <bogikidd@gmail.com>
2019-06-11 13:07:06 +0200

    Update Divide_Integers.py

--- Bit Manipulation/Bit play/Divide_Integers.py
+++ Bit Manipulation/Bit play/Divide_Integers.py
@@ -1,21 +1,23 @@
 class Solution:
-    # @return an integer
-    def divide(self, dividend, divisor):
-        isNegative = (dividend<0) ^ (divisor<0)
-        dividend = abs(dividend)
-        divisor = abs(divisor)
-        count = 1
-        while dividend >= (divisor<<1):
-            divisor <<= 1
-            count <<=1
-        result = 0
-        while dividend > 0 and divisor>=1:
-            if dividend>=divisor:
-                dividend -= divisor
-                result += count
-            divisor >>= 1
-            count >>=1
-        if isNegative:
-            return -result if -result >= -2**31 else -2**31
-        else:
-            return result if result <= 2**31 - 1 else 2**31 - 1
+	# @param dividend : integer
+	# @param divisor : integer
+	# @return an integer
+	def divide(self, dividend, divisor):
+	    isNegative = (dividend < 0) ^ (divisor < 0)
+	    dividend = abs(dividend)
+	    divisor = abs(divisor)
+	    multiples = 1
+	    while dividend >= (divisor << 1) :
+	        divisor <<= 1
+	        multiples <<= 1
+	    result = 0
+	    while divisor >= 1 and multiples >= 1:
+	        if dividend >= divisor:
+	            dividend -= divisor
+	            result += multiples
+	        multiples >>= 1
+	        divisor >>= 1
+	    if isNegative:
+	        return -result if -result > -2**31 else -2**31
+	    else:
+	        return result if result < 2**31 - 1 else 2**31 - 1

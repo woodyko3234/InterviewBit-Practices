dfcdc0fa6e9ca3544486a4bde76cb99c0db97e08
Woody Yao <bogikidd@gmail.com>
2019-06-13 15:14:26 +0200

    Update Different_Bits_Sum_Pairwise.py
    make code simpler and the time complexity remain O(n * m)

--- Bit Manipulation/Bit play/Different_Bits_Sum_Pairwise.py
+++ Bit Manipulation/Bit play/Different_Bits_Sum_Pairwise.py
@@ -2,21 +2,19 @@ class Solution:
     # @param A : list of integers
     # @return an integer
     def cntBits(self, A):
-        zeros = [0] * 32
-        ones = [0] * 32
         n = len(A)
+        binary_list = [bin(i)[2:] for i in A]
+        max_len = max(len(i) for i in binary_list)
+        bin_0 = [0] * max_len
+        bin_1 = [0] * max_len
+        sumup = 0
         for i in range(n):
-            cur = A[i]
-            idx = 0
-            while cur > 0 and idx < 32:
-                if cur % 2 == 0: pass
+            x = binary_list[i].zfill(max_len)
+            for j in range(max_len):
+                if x[j] == "0":
+                    bin_0[j] += 1
                 else:
-                    ones[idx] += 1
-                idx += 1
-                cur = cur >> 1
-        
-        sum_diff = 0
-        for i in range(32):
-            zeros[i] = n - ones[i]
-            sum_diff += 2*zeros[i]*ones[i]
-        return sum_diff%(10**9+7)
+                    bin_1[j] += 1
+                if i == n-1:
+                    sumup += bin_0[j] * bin_1[j]
+        return (sumup * 2) % (10**9 + 7)

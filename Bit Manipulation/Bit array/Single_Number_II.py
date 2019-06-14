af8c6403498a7380f20d5d179b2e76bc2f4cb26f
Woody Yao <bogikidd@gmail.com>
2019-06-14 16:06:08 +0200

    Update Single_Number_II.py
    make it understandable

--- Bit Manipulation/Bit array/Single_Number_II.py
+++ Bit Manipulation/Bit array/Single_Number_II.py
@@ -2,8 +2,14 @@ class Solution:
     # @param A : tuple of integers
     # @return an integer
     def singleNumber(self, A):
-        p, q = 0, 0
+        if not A:
+            return 
+        ones = 0
+        twos = 0
         for i in range(len(A)):
-            p = q & (p ^ A[i]) 
-            q = p | (q ^ A[i])
-        return q
+            twos = twos | (ones & A[i])
+            ones = ones ^ A[i]
+            not_threes = ~(ones & twos)
+            ones = ones & not_threes
+            twos = twos & not_threes
+        return ones

5e9ae1650a5a9a5f7d63c79cd05fa155e85b55a7
Woody Yao <bogikidd@gmail.com>
2019-06-27 15:01:15 +0200

    Update Diffk.py
    make it much simpler

--- Two Pointers/Sorting/Diffk.py
+++ Two Pointers/Sorting/Diffk.py
@@ -1,42 +1,28 @@
 class Solution:
-    # @param A : list of integers
-    # @param B : integer
-    # @return an integer
-    def diffPossible(self, A, B):
-        '''
-        whenever B + A[j] = A[i], return True
-        A is sorted, if 0 <= i < j <= n-1, then A[0] <= A[i] <= A[j] <= A[n-1]
-        do we need to take care of B is positive or negative?
-        if B < 0: find B + A[j] = A[i]
-        elif B > 0: find B + A[i] = A[j]
-        elif B == 0: find A[i] = A[j]
-        '''
-        n = len(A)
-        if n == 0: return 0
-        first = 0
-        second = 1
-        while first < (n-1) and second < n:
-            #second must be greater than first
-            if second <= first:
-                second += 1
-            elif B > 0:
-                if B + A[first] > A[second]:
-                    second += 1
-                elif B + A[first] < A[second]:
-                    first += 1
-                else:
-                    return 1
-            elif B < 0:
-                if B + A[second] > A[first]:
-                    first += 1
-                elif B + A[second] < A[first]:
-                    second += 1
-                else:
-                    return 1
-            else:
-                if A[first] < A[second]:
-                    first += 1
-                else:
-                    #in a sorted array, A[first] always <= A[second]
-                    return 1
+	# @param A : list of integers
+	# @param B : integer
+	# @return an integer
+	def diffPossible(self, A, B):
+	    n = len(A)
+	    i = 0
+	    j = i + 1
+	    if B > 0:
+	        neg = 1
+	    else: neg = -1
+	    while j < n:
+	        if neg * (A[j] - A[i]) == B: return 1
+	        else:
+	            diff = (A[j] - A[i]) - (neg * B)
+	            if diff < 0: 
+	                if j < n-1:
+	                    j += 1
+	                else: return 0
+	            elif i == j - 1:
+	                j += 1
+	                i += 1
+	            else: i += 1
+	    ###O(n^2) method
+	    #for i in range(n-1):
+        #    for j in range(i+1, n):
+        #        if abs(A[i] - A[j]) == abs(B): return 1
         return 0

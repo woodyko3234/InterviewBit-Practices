9eb2661472f67f4a2077d176b067ff2061f82e1f
Woody Yao <bogikidd@gmail.com>
2019-07-04 16:39:03 +0200

    Update Remove_Duplicates_from_Sorted_Array.py

--- Two Pointers/Inplace update/Remove_Duplicates_from_Sorted_Array.py
+++ Two Pointers/Inplace update/Remove_Duplicates_from_Sorted_Array.py
@@ -2,18 +2,12 @@ class Solution:
     # @param A : list of integers
     # @return an integer
     def removeDuplicates(self, A):
-        #A = list(set(A))
-        #return len(A)
-        #InterviewBit doesn't accept this solution lol
-    
-        to_update = 0
         n = len(A)
-        if n <= 1: return n
-        else:
-            for i in range(1, n):
-                if A[to_update] == A[i]: continue
-                else:
-                    to_update += 1
-                    A[to_update] = A[i]
+        to_update = 0
+        for i in range(1, n):
+            if A[i] == A[to_update]: continue
+            else:
+                A[to_update+1] = A[i]
+                to_update += 1
         A = A[:to_update+1]
         return len(A)

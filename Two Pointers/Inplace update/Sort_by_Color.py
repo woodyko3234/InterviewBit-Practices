dd653b373e01cf645f425c09f1e01889012be6d2
Woody Yao <bogikidd@gmail.com>
2019-07-07 23:56:13 +0200

    Update Sort_by_Color.py

--- Two Pointers/Inplace update/Sort_by_Color.py
+++ Two Pointers/Inplace update/Sort_by_Color.py
@@ -3,11 +3,9 @@ class Solution:
     # @return A after the sort
     def sortColors(self, A):
         n = len(A)
-        if n <= 1: return A
-        
-        sorted_A = []
-        for i in range(3):
-            for j in range(n):
-                if A[j] == i:
-                    sorted_A.append(i)
-        return sorted_A
+        reds, whites, blues = 0, 0, 0
+        for i in A:
+            if i == 0: reds += 1
+            elif i == 1: whites += 1
+            else: blues += 1
+        return [0] * reds + [1] * whites + [2] * blues

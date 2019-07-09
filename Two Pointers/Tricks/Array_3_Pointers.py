54e3ee3814859ccfb1ae38e4651345713d9eab4f
Woody Yao <bogikidd@gmail.com>
2019-07-09 10:40:07 +0200

    Update Array_3_Pointers.py

--- Two Pointers/Tricks/Array_3_Pointers.py
+++ Two Pointers/Tricks/Array_3_Pointers.py
@@ -4,26 +4,15 @@ class Solution:
     # @param C : tuple of integers
     # @return an integer
     def minimize(self, A, B, C):
-        i, j, k = 0, 0, 0
-        m, n, o = len(A), len(B), len(C)
-        min_max = None
-        while i < m and j < n and k < o:
-            cur_val = max(abs(A[i] - B[j]), abs(B[j] - C[k]), abs(C[k] - A[i]))
-            if min_max is None or cur_val < min_max:
-                min_max = cur_val
-            if cur_val == abs(A[i] - B[j]):
-                if A[i] > B[j]:
-                    j += 1
-                else:
-                    i += 1
-            elif cur_val == abs(B[j] - C[k]):
-                if B[j] > C[k]:
-                    k += 1
-                else:
-                    j += 1
-            else:
-                if C[k] > A[i]:
-                    i += 1
-                else:
-                    k += 1
+        n, m, l = len(A), len(B), len(C)
+        i, j, k = 0,0,0
+        min_max = float("inf")
+        while i < n and j < m and k < l:
+            comb = [A[i], B[j], C[k]]
+            cur_min = min(comb)
+            cur_diff = max(comb) - cur_min
+            if cur_diff < min_max: min_max = cur_diff
+            if A[i] == cur_min: i += 1
+            elif B[j] == cur_min: j += 1
+            else: k += 1
         return min_max

df597cbfffd90267189c0582737eb41eaf4ea5d7
Woody Yao <bogikidd@gmail.com>
2019-07-12 14:49:29 +0200

    Update Remove_Duplicates_from_Sorted_List_II.py

--- Linked Lists/List 2 pointer/Remove_Duplicates_from_Sorted_List_II.py
+++ Linked Lists/List 2 pointer/Remove_Duplicates_from_Sorted_List_II.py
@@ -1,39 +1,29 @@
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+#	def __init__(self, x):
+#		self.val = x
+#		self.next = None
 
 class Solution:
-    # @param A : head node of linked list
-    # @return the head node in the linked list
-    def deleteDuplicates(self, A):
-        newll = None
-        head = None
-        cur = A
-        notDuplicates = True
-        
-        while cur:
-            nextnd = cur.next
-            if nextnd != None and nextnd.val == cur.val:
-                notDuplicates = False
-            elif nextnd != None and nextnd.val != cur.val:    
-                if notDuplicates and newll == None:
-                    newll = ListNode(cur.val)
-                    head = newll
-                elif notDuplicates:
-                    newll.next = ListNode(cur.val)
-                else:
-                    notDuplicates = True
-            #nextnd == None, checking the last node
-            elif notDuplicates and newll == None: 
-                newll = ListNode(cur.val)
-                head = newll
-            elif notDuplicates:
-                newll.next = ListNode(cur.val)
-                
-            cur = nextnd
-            if newll and newll.next:
-                newll = newll.next
-        
-        return head
+	# @param A : head node of linked list
+	# @return the head node in the linked list
+	def deleteDuplicates(self, A):
+	    curr = A
+	    dupBool = False
+	    output = ListNode(curr.val) 
+	    head = output #not correct, need to push to the next
+	    if not curr.next: return head
+	    while curr:
+	        if curr.next:
+	            if curr.next.val == curr.val:
+	                dupBool = True
+	            elif dupBool == True:
+	                dupBool = False
+	            else:
+	                output.next = ListNode(curr.val)
+	                output = output.next
+	        else:
+	            if curr.val == output.val or dupBool == True: pass
+	            else: output.next = ListNode(curr.val)
+	        curr = curr.next
+	    return head.next #the very next node to head is the truely heading node

4b8f2a29c09fefc1db4d8314bbced51d0c40f1f0
Woody Yao <bogikidd@gmail.com>
2019-07-15 15:28:30 +0200

    Update Rotate_List.py

--- Linked Lists/List 2 pointer/Rotate_List.py
+++ Linked Lists/List 2 pointer/Rotate_List.py
@@ -1,47 +1,34 @@
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+#    def __init__(self, x):
+#        self.val = x
+#        self.next = None
 
 class Solution:
     # @param A : head node of linked list
     # @param B : integer
     # @return the head node in the linked list
     def rotateRight(self, A, B):
-        #compute length first
-        cur = A
-        n = 0
-        while cur:
+        n, curr = 0, A
+        while curr:
             n += 1
-            cur = cur.next
-        
-        if B % n == 0: #no rotation
-            return A
-        B = B % n    
-        #reset
-        cur = A
-        head = A
-        while cur:
-            nextnd = cur.next
-            if (n-1) == B:
-                prev = cur
-                cur = nextnd
-                prev.next = None
-                output = nextnd
+            curr = curr.next
+        if n == 0 or B % n == 0: return A
+        rotIdx = n - (B % n)
+        i, curL = 1, A
+        lh = curL
+        while curL:
+            if i != rotIdx:
+                i += 1
             else:
-                if nextnd != None:
-                    cur = nextnd
-                else:
-                    cur.next = head
-                    return output
-            n -= 1    
-'''
-A = 1->2->3->4->5->None
-B = 5, output = A;
-B = 2, output = 4->5->1->2->3->None
-
-so, prev should be recognized before cur is approached, and prev.next should be modified as None
-and when (cur.next = None) is hit, cur.next should be linked to the head node
-
-'''
+                curR = curL.next
+                curL.next = None
+            curL = curL.next
+        rh = curR
+        while curR:
+            if curR.next != None:
+                curR = curR.next
+            else: 
+                curR.next = lh
+                break
+        return rh

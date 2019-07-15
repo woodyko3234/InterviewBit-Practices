a62cc7e393f1eb1ba5315c8ee1bfdd682c8b9673
Woody Yao <bogikidd@gmail.com>
2019-07-15 10:28:49 +0200

    Update Merge_Two_Sorted_Lists.py

--- Linked Lists/List 2 pointer/Merge_Two_Sorted_Lists.py
+++ Linked Lists/List 2 pointer/Merge_Two_Sorted_Lists.py
@@ -1,37 +1,36 @@
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+#	def __init__(self, x):
+#		self.val = x
+#		self.next = None
 
 class Solution:
-    # @param A : head node of linked list
-    # @param B : head node of linked list
-    # @return the head node in the linked list
-    def mergeTwoLists(self, A, B):
-        
-        curA, curB = A, B
-        
-        output = [None, None]
-        
-        while curA and curB:
-            nextA, nextB = curA.next, curB.next
-            if curA.val > curB.val:
-                if output[0] == None:
-                    output[0] = curB
+	# @param A : head node of linked list
+	# @param B : head node of linked list
+	# @return the head node in the linked list
+	def mergeTwoLists(self, A, B):
+	    if not (A or B): return None
+	    elif not A: return B
+	    elif not B: return A
+	    currA, currB = A, B
+	    merged = None
+	    while currA and currB:
+	        if currA.val > currB.val:
+	            if merged:
+	                merged.next = ListNode(currB.val)
+	                merged = merged.next
+	            else:
+	                merged = ListNode(currB.val)
+	                head = merged
+	            currB = currB.next
+	        else:
+	            if merged:
+                    merged.next = ListNode(currA.val)
+                    merged = merged.next
                 else:
-                    output[1].next = curB
-                output[1] = curB
-                curB = nextB
-            else:
-                if output[0] == None:
-                    output[0] = curA
-                else:
-                    output[1].next = curA
-                output[1] = curA
-                curA = nextA
-        if curA:
-            output[1].next = curA
-        else:
-            output[1].next = curB
-        return output[0]
+                    merged = ListNode(currA.val)
+                    head = merged
+                currA = currA.next
+	    if currA: merged.next = currA
+	    else: merged.next = currB
+	    return head

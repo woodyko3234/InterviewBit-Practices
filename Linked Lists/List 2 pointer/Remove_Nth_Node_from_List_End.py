a9312b94c808cea82691570d80a1a9ddbf675a85
Woody Yao <bogikidd@gmail.com>
2019-07-15 13:36:40 +0200

    Update Remove_Nth_Node_from_List_End.py

--- Linked Lists/List 2 pointer/Remove_Nth_Node_from_List_End.py
+++ Linked Lists/List 2 pointer/Remove_Nth_Node_from_List_End.py
@@ -1,46 +1,28 @@
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+#    def __init__(self, x):
+#        self.val = x
+#        self.next = None
 
 class Solution:
     # @param A : head node of linked list
     # @param B : integer
     # @return the head node in the linked list
     def removeNthFromEnd(self, A, B):
-        cur = A
-        n = 0
-        #compute the length
-        while cur:
+        n, curr = 0, A
+        while curr:
             n += 1
-            cur = cur.next
-        
-        if n <= B:
-            A = A.next
-            return A
-        #reset    
-        cur = A
-        
-        while cur:
-            nextnd = cur.next
-            if n != B:
-                prev = cur
+            curr = curr.next
+        if n <= B: return A.next
+        toDrop, i, curr = n - B, 1, A
+        head = curr
+        while curr:
+            if i != toDrop:
+                i += 1
+            elif B == 1 and i == n - 1:
+                curr.next = None
             else:
-                prev.next = nextnd #skip cur
-            cur = nextnd
-            n -= 1
-        return A
-'''
-A = 1->2->3->4->5
-B = 5, output = 2->3->4->5
-first while loop => n = 5
-cur = LN(1)
-nextnd = LN(2)
-def solved the B=5
-e.g. B = 2, output = 1->2->3->5
-cur = LN(1)
-nextnd = LN(2)
-when n = 2: cur = LN(4)
-'''
-        
+                curr.next = curr.next.next
+                break
+            curr = curr.next
+        return head

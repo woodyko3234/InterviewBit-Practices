9025159c7274e2e12ca2b41f17693ff56211be9e
Woody Yao <bogikidd@gmail.com>
2019-07-23 14:08:02 +0200

    Update Insertion_Sort_List.py

--- Linked Lists/List sort/Insertion_Sort_List.py
+++ Linked Lists/List sort/Insertion_Sort_List.py
@@ -1,23 +1,36 @@
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+#    def __init__(self, x):
+#        self.val = x
+#        self.next = None
 
 class Solution:
     # @param A : head node of linked list
     # @return the head node in the linked list
     def insertionSortList(self, A):
-        cur = A
-        values = []
-        while cur:
-            values.append(cur.val)
-            cur = cur.next
-        
-        values.sort()
-        
-        output = looper = ListNode(values[0])
-        for i in values[1:]:
-            looper.next = ListNode(i) or None
-            looper = looper.next
-        return output
+        if not A: return A
+        curr, sortedLL = A.next, ListNode(A.val)
+        sLL_h = sortedLL
+        while curr:
+            prev = None
+            while sortedLL:
+                if sortedLL.val < curr.val:
+                    prev = sortedLL
+                    if sortedLL.next:
+                        sortedLL = sortedLL.next
+                    else:
+                        sortedLL.next = ListNode(curr.val)
+                        break
+                elif prev:
+                    prev.next = ListNode(curr.val)
+                    prev.next.next = sortedLL
+                    break
+                else:
+                    prev = ListNode(curr.val)
+                    prev.next = sLL_h
+                    sLL_h = prev
+                    break
+            sortedLL = sLL_h
+            curr = curr.next
+        return sLL_h
+                

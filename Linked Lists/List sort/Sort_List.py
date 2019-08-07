f697ed5c9287320e6e79bb12fbed6f9433d4d40b
Woody Yao <bogikidd@gmail.com>
2019-08-07 14:55:52 +0200

    Update Sort_List.py

--- Linked Lists/List sort/Sort_List.py
+++ Linked Lists/List sort/Sort_List.py
@@ -1,59 +1,57 @@
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+#	def __init__(self, x):
+#		self.val = x
+#		self.next = None
 
 class Solution:
-    # @param A : head node of linked list
-    # @return the head node in the linked list
-    def sortList(self, A):
-        nodes = []
-        while A:
-            nodes.append(A)
-            A = A.next
-        nodes.sort(key=lambda x: x.val)
-        head = ListNode('dummy')
-        result = head
-
-        for node in nodes:
-            head.next = node
-            head = head.next
-
-        head.next = None
-        return result.next
+	# @param A : head node of linked list
+	# @return the head node in the linked list
+	def sortList(self, A):
+	    '''Merge Sort is recommended.
+	       step1. break the linked list into small lists
+	       step2. sort the small lists one by one
+	       step3. compare and combine the small lists.'''
+	    if not A.next: return A
+	    mid = self.getMid(A)
+        next_of_mid = mid.next
+        mid.next = None
         
+        left = self.sortList(A)
+        right = self.sortList(next_of_mid)
+        
+        result = self.mergeSort(left, right)
+        
+        return result
 
-
-
-'''
-A = 5->4->2->1
-head = LN(5):
-cur = LN(5), nextnd = LN(4), next2 = LN(2)
-1. 
-nextnd.next = cur
-cur.next = next2
-4->5->2->1
-nextnd = next2, next2 = next2.next
-cur = LN(5), nextnd = LN(2), next2 = LN(1)
-2.
-nextnd.next = cur
-cur.next = next2
-4->2->5->1
-nextnd = next2, next2 = next2.next
-cur = LN(5), nextnd = LN(1), next2 = None
-3.
-nextnd.next = cur
-cur.next = next2
-4->2->1->5
-if next2 == None: back and go again
-
-4.
-cur = LN(4), nextnd = LN(2), next2 = LN(1)
-2->4->1->5
-5.
-2->1->4->5
-
-6.
-1->2->4->5
-'''
+        
+    def getMid(self, A):
+        """get the mid point with while loop"""
+        if not A or not A.next: return A
+        mid, curr = A, A.next
+        while curr:
+            curr = curr.next
+            if curr:
+                mid = mid.next
+                curr = curr.next
+        return mid
+    
+    def mergeSort(self, left, right):
+        """apply merge sort into the separated linked lists"""
+        if not left: return right
+        elif not right: return left
+        head = ListNode(float("-inf"))
+        output = head
+        while left and right:
+            if left.val > right.val:
+                output.next = ListNode(right.val)
+                right = right.next
+            else:
+                output.next = ListNode(left.val)
+                left = left.next
+            output = output.next
+        if left:
+            output.next = left
+        elif right:
+            output.next = right
+        return head.next

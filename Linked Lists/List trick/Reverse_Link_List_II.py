6c86da3e274625ea9c754fa48dc537639d420235
Woody Yao <bogikidd@gmail.com>
2019-08-07 17:01:14 +0200

    Update Reverse_Link_List_II.py

--- Linked Lists/List trick/Reverse_Link_List_II.py
+++ Linked Lists/List trick/Reverse_Link_List_II.py
@@ -1,44 +1,40 @@
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+#	def __init__(self, x):
+#		self.val = x
+#		self.next = None
 
 class Solution:
-    # @param A : head node of linked list
-    # @param m : integer
-    # @param n : integer
-    # @return the head node in the linked list
-    def reverseBetween(self, A, m, n):
-        if m == n: return A
-        cur = A
-        length = 0
-        prev = None
-        head = A
-        while cur:
-            length += 1
-            nextnd = cur.next
-            if length < m:
-                prev = cur
-                cur = nextnd
-            else: #length >= m
-                lnList = []
-                for i in range(m, n+1):
-                    lnList.append(cur.val)
-                    cur = cur.next
-                if prev == None: #m = 1
-                    head = ListNode(lnList[-1])
-                    prev = head
-                    for i in lnList[-2::-1]:
-                        prev.next = ListNode(i)
-                        prev = prev.next
-                    prev.next = cur
-                    return head
-                else:
-                    for i in lnList[::-1]:
-                        prev.next = ListNode(i)
-                        prev = prev.next
-                    prev.next = cur
-                    return head
-
-        return head
+	# @param A : head node of linked list
+	# @param m : integer
+	# @param n : integer
+	# @return the head node in the linked list
+	def reverseBetween(self, A, m, n):
+	    if m == n: return A
+	    i = 0
+	    head = ListNode(0)
+	    head.next = A
+	    curr = head
+	    while curr:
+	        if i == m-1:
+	            cur_end = curr
+	        elif i == m:
+	            start = curr
+	            curr = curr.next
+	            i += 1
+	            start.next = None
+	            temp = start
+	            continue
+	        elif n >= i > m:
+	            cur_end.next = curr
+	            curr = curr.next
+	            cur_end.next.next = temp
+	            temp = cur_end.next
+	            if i == n:
+	                start.next = curr
+	                break
+                i += 1
+	            continue
+	        curr = curr.next
+	        i += 1
+        return head.next

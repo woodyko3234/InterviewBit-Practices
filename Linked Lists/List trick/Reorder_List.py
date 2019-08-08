6ef3e9f1bd0f26d3f5dfd157952221e11df5c25a
Woody Yao <bogikidd@gmail.com>
2019-08-08 14:39:38 +0200

    Update Reorder_List.py

--- Linked Lists/List trick/Reorder_List.py
+++ Linked Lists/List trick/Reorder_List.py
@@ -1,33 +1,46 @@
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+#	def __init__(self, x):
+#		self.val = x
+#		self.next = None
 
 class Solution:
-    # @param A : head node of linked list
-    # @return the head node in the linked list
-    def reorderList(self, A):
-        if A == None: return None
-        cur = A.next
-        vallist = []
-        while cur:
-            vallist.append(cur.val)
-            cur = cur.next
+	# @param A : head node of linked list
+	# @return the head node in the linked list
+	def reorderList(self, A):
+	    leftLL, rightLL = self.divideList(A)
+	    if not rightLL: return leftLL
+	    rightLL = self.reverseList(rightLL)
+	    head = ListNode(0)
+	    curr = head
+	    leftN, rightN = leftLL, rightLL
+	    while rightN:
+	        curr.next = leftN
+	        curr, leftN = curr.next, leftN.next
+	        curr.next = rightN
+	        curr, rightN = curr.next, rightN.next
+        curr.next = leftN
+        return head.next
+	    
+	    
+    def divideList(self, A):
+        curr, left, mid = A, A, A
+        while curr:
+            curr = curr.next
+            if curr:
+                curr = curr.next
+                mid = mid.next
+        right = mid.next
+        mid.next = None
+        return left, right
         
-        n = len(vallist)
-        m = len(vallist)
-        head = ListNode(A.val)
-        cur = head
-        i = 0
-        while n >= 2:
-            cur.next = ListNode(vallist[(m-1)-i])
-            cur = cur.next
-            cur.next = ListNode(vallist[i])
-            cur = cur.next
-            n -= 2
-            i += 1
-        if n == 1:
-            cur.next = ListNode(vallist[i])
-        return head
-            
+    def reverseList(self, right):
+        if not right.next: return right
+        head = ListNode(0)
+        curr, temp = right.next, ListNode(right.val)
+        while curr:
+            head.next = curr
+            curr = curr.next
+            head.next.next = temp
+            temp = head.next
+        return head.next

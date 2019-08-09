dfea6993fbc9796630fb38a4a9397c62e43ca4d1
Woody Yao <bogikidd@gmail.com>
2019-08-09 08:36:37 +0200

    Update K_reverse_linked_list.py

--- Linked Lists/Pointer move/K_reverse_linked_list.py
+++ Linked Lists/Pointer move/K_reverse_linked_list.py
@@ -1,28 +1,31 @@
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+#	def __init__(self, x):
+#		self.val = x
+#		self.next = None
 
 class Solution:
-    # @param A : head node of linked list
-    # @param B : integer
-    # @return the head node in the linked list
-    def reverseList(self, A, B):
-        if B == 1: return A
-        cur = A
-        output = []
-        times = 0
-        while cur != None:
-            groups = [None]*(B+1)
-            for i in range(B):
-                after = cur.next
-                groups[i] = cur
-                groups[i].next = groups[i-1]
-                cur = after
-            output.append(groups)
-            times += 1
-        for i in range(times-1):
-            output[i][0].next = output[i+1][B-1]
-        return output[0][B-1]
-            
+	# @param A : head node of linked list
+	# @param B : integer
+	# @return the head node in the linked list
+	def reverseList(self, A, B):
+	    if B <= 1: return A
+	    curr, i, temp = A, 0, None
+	    head = ListNode(0)
+	    head.next = A
+	    link_head = head
+	    while curr:
+	        i += 1
+	        if i == 1:
+	            temp_end = curr
+	        temp_head = curr
+	        curr = curr.next
+	        temp_head.next = temp
+	        temp = temp_head
+	        if i == B:
+	            i = 0
+	            link_head.next = temp_head
+	            temp = None
+	            temp_end.next = curr
+	            link_head = temp_end
+        return head.next

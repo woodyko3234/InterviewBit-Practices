ff13d92f312e9b8cb353b4447b0d0fff577e0c3f
Woody Yao <bogikidd@gmail.com>
2019-09-17 16:46:21 +0200

    Update Evaluate_Expression.py

--- Stacks And Queues/Stack math/Evaluate_Expression.py
+++ Stacks And Queues/Stack math/Evaluate_Expression.py
@@ -2,25 +2,20 @@ class Solution:
 	# @param A : list of strings
 	# @return an integer
 	def evalRPN(self, A):
-        """
-        [55, 2, 3, +, /] => (55)/((2)+(3))
-        [2, 1, +, 3, *] => ((2)+(1))*(3)
-        [55, *, 2, +, 3] => Invalid
-        """
-        toOperate = []
-        operators = ["+", "-", "*", "/"]
-        for e in A:
-            if e not in operators:
-                toOperate.append(int(e))
-            else:
-                opTwo = toOperate.pop()
-                opOne = toOperate.pop()
-                if e == "+":
-                    toOperate.append(opOne + opTwo)
-                elif e == "-":
-                    toOperate.append(opOne - opTwo)
-                elif e == "*":
-                    toOperate.append(opOne * opTwo)
-                elif e == "/":
-                    toOperate.append(opOne // opTwo)
-        return toOperate.pop()
+		toOperate = []
+		operators = ["+", "-", "*", "/"]
+		for e in A:
+			if e not in operators:
+				toOperate.append(int(e))
+			else:
+				opTwo = toOperate.pop()
+				opOne = toOperate.pop()
+				if e == "+":
+					toOperate.append(opOne + opTwo)
+				elif e == "-":
+					toOperate.append(opOne - opTwo)
+				elif e == "*":
+					toOperate.append(opOne * opTwo)
+				elif e == "/":
+					toOperate.append(opOne // opTwo)
+		return toOperate.pop()

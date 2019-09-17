fee650f23c06f9b21d90b0feffec094e004b3fd5
Woody Yao <bogikidd@gmail.com>
2019-09-17 15:50:00 +0200

    Update Evaluate_Expression.py
    modify and apply stacking technique

--- Stacks And Queues/Stack math/Evaluate_Expression.py
+++ Stacks And Queues/Stack math/Evaluate_Expression.py
@@ -1,29 +1,26 @@
-'''
-  ["2", "1", "+", "3", "*"] -> ((2 + 1) * 3) -> 9
-  ["4", "13", "5", "/", "+"] -> (4 + (13 / 5)) -> 6
-  [ "5", "1", "2", "+", "4", "*", "+", "3", "-" ] -> (5 + ((1+2) * 4) - 3) = 14
-'''
-
 class Solution:
-    # @param A : list of strings
-    # @return an integer
-    def evalRPN(self, A):
-        manipulation = []
-        for i in A:
-            try:
-                cur = int(i)
-                manipulation.append(cur)
-            except:
-                if i == '+':
-                    comp = manipulation[-2] + manipulation[-1]
-                elif i == '-':
-                    comp = manipulation[-2] - manipulation[-1]
-                elif i == '*':
-                    comp = manipulation[-2] * manipulation[-1]
-                elif i == '/':
-                    comp = manipulation[-2] / manipulation[-1]
-                else:
-                    return -1
-                manipulation[-2] = comp
-                manipulation.pop()
-        return manipulation[0]
+	# @param A : list of strings
+	# @return an integer
+	def evalRPN(self, A):
+        """
+        [55, 2, 3, +, /] => (55)/((2)+(3))
+        [2, 1, +, 3, *] => ((2)+(1))*(3)
+        [55, *, 2, +, 3] => Invalid
+        """
+        toOperate = []
+        operators = ["+", "-", "*", "/"]
+        for e in A:
+            if e not in operators:
+                toOperate.append(int(e))
+            else:
+                opTwo = toOperate.pop()
+                opOne = toOperate.pop()
+                if e == "+":
+                    toOperate.append(opOne + opTwo)
+                elif e == "-":
+                    toOperate.append(opOne - opTwo)
+                elif e == "*":
+                    toOperate.append(opOne * opTwo)
+                elif e == "/":
+                    toOperate.append(opOne // opTwo)
+        return toOperate.pop()

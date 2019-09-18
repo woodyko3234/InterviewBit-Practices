ee79e767744c4e002f2d29aa7e559b4d31dd6834
Woody Yao <bogikidd@gmail.com>
2019-09-18 15:31:44 +0200

    Update Min_Stack.py

--- Stacks And Queues/Multiple stack/Min_Stack.py
+++ Stacks And Queues/Multiple stack/Min_Stack.py
@@ -1,39 +1,26 @@
 class MinStack:
-    stack = []
-    minimums = []
-    # @param x, an integer
-    # @return an integer
-    def push(self, x):
-        if len(self.stack) == 0:
-            self.stack.append(x)
-            self.minimums.append(x)
-        else:
-            self.stack.append(x)
-            if x < self.minimums[-1]:
-                self.minimums.append(x)
-            else:
-                self.minimums.append(self.minimums[-1])
-
-    # @return nothing
-    def pop(self):
-        if len(self.stack) != 0:
-            self.stack.pop()
-            self.minimums.pop()
-
-    # @return an integer
-    def top(self):
-        if len(self.stack) == 0:
-            return -1
-        else:
-            return self.stack[-1]
-
-    # @return an integer
-    def getMin(self):
-        if len(self.stack) == 0:
-            return -1
-        else:
-            return self.minimums[-1]
-    
     def __init__(self):
         self.stack = []
         self.minimums = []
+    # @param x, an integer
+    def push(self, x):
+        self.stack.append(x)
+        self.minimums.append(x)
+        if len(self.minimums) >= 2 and x > self.minimums[-2]:
+            self.minimums[-1] = self.minimums[-2]
+    # @return nothing
+    def pop(self):
+        if self.stack:
+            self.stack.pop()
+            self.minimums.pop()
+        else: return -1
+    # @return an integer
+    def top(self):
+        if self.stack:
+            return self.stack[-1]
+        return -1
+    # @return an integer
+    def getMin(self):
+        if self.minimums:
+            return self.minimums[-1]
+        return -1

520ea5f6328e1605332481a1190a6ec6b8765321
Woody Yao <bogikidd@gmail.com>
2019-09-18 16:20:52 +0200

    Create Sliding_Window_Maximum.py

---
+++ Stacks And Queues/Cleverstack/Sliding_Window_Maximum.py
@@ -0,0 +1,22 @@
+import collections
+class Solution:
+    # @param {integer[]} nums
+    # @param {integer} k
+    # @return {integer[]}
+    def slidingMaximum(self, nums, k):
+        res = []
+        d = collections.deque()
+        
+        for i, n in enumerate(nums):
+            while d and nums[d[-1]] < n:
+                d.pop()
+                
+            d.append(i)
+            
+            if d[0] == i - k:
+                d.popleft()
+            
+            if i >= k - 1:
+                res.append(nums[d[0]])
+        
+        return res 

a2b4281b8c29bbce82728e996cfcc5d4927b3fc7
Woody Yao <bogikidd@gmail.com>
2019-11-08 14:28:45 +0200

    Update Combination_Sum.py
    reformatted and removed unnecessary codes

--- Backtracking/Subsets/Combination_Sum.py
+++ Backtracking/Subsets/Combination_Sum.py
@@ -1,27 +1,25 @@
 class Solution:
-	# @param c : list of integers
-	# @param t : integer
-	# @return a list of list of integers
-  def __init__(self):
-	  self.output = []
-	    
-	def sortList(self, c):
-	  return sorted(list(set(c)))
-	    
-	def combinationSum(self, c, t, pos = 0, temp = None):
-	  temp = temp or []
-	  c = self.sortList(c)
-	    
-	  if t == 0: 
-      self.output.append(temp[:])
-      return 
-    elif t < 0:
-      return
-    else:
-      for i in range(pos, len(c)):
-        temp.append(c[i])
-        self.combinationSum(c, t-c[i], i, temp)
-        temp.pop()
-    if len(temp) > 0: 
-      return
-    else: return self.output
+    # @param c : list of integers
+    # @param t : integer
+    # @return a list of list of integers
+    def __init__(self):
+        self.output = []
+        
+    def sortList(self, c):
+        return sorted(list(set(c)))
+        
+    def combinationSum(self, c, t, pos = 0, temp = None):
+        temp = temp or []
+        c = self.sortList(c)
+        
+        if t == 0: 
+            self.output.append(temp[:])
+            return 
+        elif t < 0:
+            return
+        else:
+            for i in range(pos, len(c)):
+                temp.append(c[i])
+                self.combinationSum(c, t-c[i], i, temp)
+                temp.pop()
+        return self.output

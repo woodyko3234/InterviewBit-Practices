08a0e04550d310b01d196eacd91eb984d0e20e03
Woody Yao <bogikidd@gmail.com>
2019-11-15 11:27:29 +0200

    Update Kth_Permutation_Sequence.py
    updated wording and codes to make it easier to understand and more quickly to run.

--- Backtracking/Maths and backtracking/Kth_Permutation_Sequence.py
+++ Backtracking/Maths and backtracking/Kth_Permutation_Sequence.py
@@ -1,46 +1,57 @@
 class Solution:
-    # @param A : integer
-    # @param B : integer
-    # @return a strings
-    def getPermutation(self, n, k):
-        '''
-        example arr[1,2,3], n = 3
+	# @param n : integer
+	# @param k : integer, must be less than n! and greater than 0
+	# @return a strings
+	def __init__(self):
+	    self.output = ""
+	    
+    def factorial(self, n, facList):
+        """
+        apply a list to store those factorials, avoiding duplicated computations.
+        """
+        if n > 1:
+            if facList[-n] == -1:
+                facList[-n] = n * self.factorial(n-1, facList)
+            return n*self.factorial(n-1, facList)
+        else: 
+            if facList[-n] == -1:
+                facList[-n] = n
+            return n
+	    
+	def getPermutation(self, n, k):
+	    '''
+        example: arr = [1,2,3], n = 3
         3 choice for index_0
         2 for index_1
         1 for index_2 (no choice)
+        so total permutations = 3! = 6
         if k = 4
-        index0 = n.pop((k-1) // (n-1)!) #3//2 = 1, n.pop(1) = 2, n := [1,3]
-        new k = k - (n-1)! * pop_val #4-2*2 = 2
-        index1 = n.pop((k-1) // (n-1)!) #1//1 = 1, n.pop(1) = 3, n := [1]
-        if len(n) == 1: index_-1 = *n
+        index0 = arr.pop((k-1) // (n-1)!) #3//2 = 1, arr.pop(1) = 2, arr := [1,3]
+        new k = k - (n-1)! * popped_idx #4-2*1 = 2
+        index1 = arr.pop((k-1) // (n-1)!) #1//1 = 1, arr.pop(1) = 3, arr := [1]
+        if len(arr) == 1: index_-1 = arr.pop()
         
-        how about arr[1,2,3,4] n = 4 and k = 10?
-        index0 = n.pop((k-1) // (n-1)!) #9//6 = 1, n.pop(1) = 2, n := [1,3,4]
-        new k = k - (n-1)! * pop_val #10-6*1 = 4 new n = 3
-        index1 = n.pop((k-1) // (n-1)!) #3//2 = 1, n.pop(1) = 3, n := [1,4]
-        new k = k - (n-1)! * pop_val #4-2*1 = 2 new n = 2
-        index2 = n.pop((k-1) // (n-1)!) #1//1 = 1, n.pop(1) = 4, n := [1]
+        how about arr = [1,2,3,4] n = 4 and k = 10?
+        index0 = arr.pop((k-1) // (n-1)!) #9//6 = 1, arr.pop(1) = 2, arr := [1,3,4]
+        new k = k - (n-1)! * popped_idx #10-6*1 = 4 new n = 3
+        index1 = arr.pop((k-1) // (n-1)!) #3//2 = 1, arr.pop(1) = 3, arr := [1,4]
+        new k = k - (n-1)! * popped_idx #4-2*1 = 2 new n = 2
+        index2 = arr.pop((k-1) // (n-1)!) #1//1 = 1, arr.pop(1) = 4, arr := [1]
         ans:2341
         '''
-        #How can we solve it without applying factorial???
-        array = list(map(str,range(1, n+1)))
-        output = ""
-        combinations = 1
-        new_k = k
-        l = len(array)
-        def factorial(A):
-            if A == 0:
-                return 1
-            return A*factorial(A-1)
-        combinations = factorial(n-1)
-        while l > 1:
-            pop_val = int((new_k-1) // (combinations))
-            output+=array[pop_val]
-            array.remove(array[pop_val])
-            new_k -= combinations * pop_val
-            l -= 1
-            combinations = int(combinations / l) 
-            #apply int to avoid it become float when testing with long numbers 
-        output += array[0]
-        return output
+	    if n == 1: return "1"
+        arr = [str(i+1) for i in range(n)]
+        facList = [-1] * (n-1)
+        self.factorial(n-1, facList)
+        facList = facList[::-1]
+        def loopFunc(arr, n, k, facList):
+            while arr and facList:
+                fac_val = facList.pop()
+                idx = (k-1) // fac_val
+                self.output += arr.pop(idx)
+                n -= 1
+                k -= fac_val * idx
+            self.output += arr.pop()
         
+        loopFunc(arr, n, k, facList)
+        return self.output

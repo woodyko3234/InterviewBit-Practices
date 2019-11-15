a0eaca99f7e6a84629fe93309049ce4ff5099b16
Woody Yao <bogikidd@gmail.com>
2019-11-15 11:31:40 +0200

    Update Kth_Permutation_Sequence.py
    indention adjusted, removed variable n in loopFunc since it was unnecessary

--- Backtracking/Maths and backtracking/Kth_Permutation_Sequence.py
+++ Backtracking/Maths and backtracking/Kth_Permutation_Sequence.py
@@ -1,10 +1,10 @@
 class Solution:
-	# @param n : integer
-	# @param k : integer, must be less than n! and greater than 0
-	# @return a strings
-	def __init__(self):
-	    self.output = ""
-	    
+    # @param n : integer
+    # @param k : integer, must be less than n! and greater than 0
+    # @return a strings
+    def __init__(self):
+        self.output = ""
+        
     def factorial(self, n, facList):
         """
         apply a list to store those factorials, avoiding duplicated computations.
@@ -17,9 +17,9 @@ class Solution:
             if facList[-n] == -1:
                 facList[-n] = n
             return n
-	    
-	def getPermutation(self, n, k):
-	    '''
+        
+    def getPermutation(self, n, k):
+        '''
         example: arr = [1,2,3], n = 3
         3 choice for index_0
         2 for index_1
@@ -39,17 +39,16 @@ class Solution:
         index2 = arr.pop((k-1) // (n-1)!) #1//1 = 1, arr.pop(1) = 4, arr := [1]
         ans:2341
         '''
-	    if n == 1: return "1"
+        if n == 1: return "1"
         arr = [str(i+1) for i in range(n)]
         facList = [-1] * (n-1)
         self.factorial(n-1, facList)
         facList = facList[::-1]
-        def loopFunc(arr, n, k, facList):
+        def loopFunc(arr, k, facList):
             while arr and facList:
                 fac_val = facList.pop()
                 idx = (k-1) // fac_val
                 self.output += arr.pop(idx)
-                n -= 1
                 k -= fac_val * idx
             self.output += arr.pop()
         

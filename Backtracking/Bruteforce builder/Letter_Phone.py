79daef0004d39f51947af4f902e806b6c80c5f40
Woody Yao <bogikidd@gmail.com>
2019-11-15 15:41:52 +0200

    Update Letter_Phone.py
    simplified the codes

--- Backtracking/Bruteforce builder/Letter_Phone.py
+++ Backtracking/Bruteforce builder/Letter_Phone.py
@@ -1,26 +1,23 @@
 class Solution:
-    # @param A : string
+    # @param s : string
     # @return a list of strings
-    def letterCombinations(self, A):
-        letters = ""
+    def __init__(self):
+        #build the mapping dict
+        self.numPad = {"0": ["0"], "1": ["1"], "2": ["a", "b", "c"],
+                     "3": ["d", "e", "f"], "4": ["g", "h", "i"], 
+                     "5": ["j", "k", "l"], "6": ["m", "n", "o"],
+                     "7": ["p", "q", "r", "s"], "8": ["t", "u", "v"],
+                     "9": ["w", "x", "y", "z"]}
+
+    def letterCombinations(self, s):
         comb = []
-        alphabets = {'0': ['0'], '1': ['1'], '2':['a','b','c'], 
-                  '3':['d','e','f'], '4':['g','h','i'],
-                  '5':['j','k','l'], '6':['m','n','o'],
-                  '7':['p','q','r','s'], '8':['t','u','v'],
-                  '9':['w','x','y','z']}
-        i = 0
-        n = len(A)
-        
-        self.recursive(A,i,n,letters,comb,alphabets)
-        
+        temp = ""
+        def recursive(temp, s):
+            if len(s) > 1:
+                for i in self.numPad[s[0]]:
+                    recursive(temp + i, s[1:])
+            else:
+                for i in self.numPad[s[0]]:
+                    comb.append(temp + i)
+        recursive(temp, s)
         return comb
-        
-        
-    def recursive(self, A, i, n, letters, comb, alphabets):
-        if i < n:
-            for choice in alphabets[A[i]]:
-                self.recursive(A, i+1, n, letters+choice, comb, alphabets)
-        if i == n: #create unnecessary strings, how to avoid?
-            comb.append(letters)
-        return 

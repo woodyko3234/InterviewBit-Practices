ceac11a1f2f75edd29be0240ff1b0d57002452ac
Woody Yao <bogikidd@gmail.com>
2019-11-18 16:42:56 +0200

    Update Generate_all_Parentheses_II_py27.py
    made it simpler

--- Backtracking/Bruteforce builder/Generate_all_Parentheses_II_py27.py
+++ Backtracking/Bruteforce builder/Generate_all_Parentheses_II_py27.py
@@ -1,24 +1,21 @@
 class Solution:
-    # @param A : integer
-    # @return a list of strings
-    def generateParenthesis(self, A):
-        left = 0
-        right = 0
-        word = ""
-        comb = []
-        
-        self.recursive(A, word, comb, left, right)
-        
-        return comb
-        
-    def recursive(self, A, word, comb, left, right):
-        if left == 0 or (left == right and left < A):
-            self.recursive(A, word + '(', comb, left + 1, right)
-        elif A > left > 0 and left > right:
-            self.recursive(A, word + '(', comb, left + 1, right) or \
-            self.recursive(A, word + ')', comb, left, right + 1)
-        elif left == A and right < A:
-            self.recursive(A, word + ')', comb, left, right + 1)
-        if left + right == 2*A:
-            comb.append(word)
-        return 
+	# @param A : integer
+	# @return a list of strings
+	def generateParenthesis(self, A):
+	    ptsComb = []
+	    if A == 0: return ptsComb
+	    leftPts, rightPts = 0, 0
+	    
+	    def recursive(leftPts, rightPts, A, curr):
+	        if rightPts == A:
+                ptsComb.append(curr)
+	        elif leftPts == rightPts:
+	            recursive(leftPts+1, rightPts, A, curr + "(")
+	        elif leftPts < A:
+	            recursive(leftPts+1, rightPts, A, curr + "(")
+	            recursive(leftPts, rightPts + 1, A, curr + ")")
+	        elif rightPts < A:
+	            recursive(leftPts, rightPts+1, A, curr + ")")
+	    
+	    recursive(leftPts, rightPts, A, "")
+	    return ptsComb

22c2d4438761563c5a6f8233fdd049bf1a1b8f8c
Woody Yao <bogikidd@gmail.com>
2020-01-02 15:18:26 +0200

    Update 4_Sum.py

--- Hashing/Hash search/4_Sum.py
+++ Hashing/Hash search/4_Sum.py
@@ -1,27 +1,32 @@
 from collections import defaultdict
-
 class Solution:
-    # @param A : list of integers
-    # @param B : integer
+    # @param S : list of integers
+    # @param T : integer
     # @return a list of list of integers
-    def fourSum(self, A, B):
-        #create a dict to store a+b info
-        sum_ab = defaultdict(list)
-        n = len(A)
-        for i in range(n-1):
-            for j in range(i+1, n):
-                sum_ab[A[i] + A[j]].append([i,j])
-        
-        #create another dict to store B-(a+b) info
-        sum_cd = defaultdict(list)
-        comb = []
-        for i in range(2, n-1):
-            for j in range(i+1, n):
-                if sum_ab[B-(A[i]+A[j])]:
-                    for k in sum_ab[B-(A[i]+A[j])]:
-                        if i not in k and j not in k:
-                            comb.append(tuple(sorted([A[k[0]], A[k[1]], A[i],  A[j]])))
-        comb = list(set(comb))
-        
-        return sorted(comb)
-            
+    def fourSum(self, S, T):
+        #since condition a<=b<=c<=d must be applied
+        S.sort()
+        sumMatch = defaultdict(list)
+        n, output = len(S), []
+        for i in range(n-3):
+            for j in range(i+1, n-2):
+                #T-(S[i]+S[j]) == S[k]+S[l]
+                if [S[i], S[j]] in sumMatch[T-S[i]-S[j]]:
+                    continue
+                sumMatch[T-S[i]-S[j]].append([S[i], S[j]])
+                target = T-S[i]-S[j]
+                k, l = j+1, n-1
+                while k < l:
+                    if target-S[k]-S[l] > 0:
+                        k += 1
+                    elif target-S[k]-S[l] < 0:
+                        l -= 1
+                    else:
+                        output.append([S[i],S[j],S[k],S[l]])
+                        while S[k] == S[k+1] and k+1 < l:
+                            k += 1
+                        while S[l] == S[l-1] and l-1 > k:
+                            l -= 1
+                        k += 1
+                        l -= 1
+        return output

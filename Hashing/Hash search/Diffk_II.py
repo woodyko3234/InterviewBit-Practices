5e5ce79846f621b2e50444a68f9ae3a4b717293f
Woody Yao <bogikidd@gmail.com>
2020-01-07 15:23:26 +0200

    Update Diffk_II.py

--- Hashing/Hash search/Diffk_II.py
+++ Hashing/Hash search/Diffk_II.py
@@ -1,13 +1,13 @@
 class Solution:
-	# @param A : tuple of integers
-	# @param k : integer, non negative
-	# @return an integer
+    # @param A : tuple of integers
+    # @param k : integer, non negative
+    # @return an integer
     def diffPossible(self, A, k):
-	    """
-	    i can be greater or smaller than j
-	    so the condition can be modified as
-	    |A[i] - A[j]| = k
-	    """
+        """
+        i can be greater or smaller than j
+        so the condition can be modified as
+        |A[i] - A[j]| = k
+        """
         checklist, n = dict(), len(A)
         for i in range(n):
             if A[i] > k:

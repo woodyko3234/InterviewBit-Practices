c25ef0792be02a9e674a97d8e0d3d874afaa120a
Woody Yao <bogikidd@gmail.com>
2020-01-22 10:19:44 +0200

    Update Fraction.py

--- Hashing/Maths and hashing/Fraction.py
+++ Hashing/Maths and hashing/Fraction.py
@@ -1,26 +1,31 @@
+from collections import defaultdict
 class Solution:
-    # @param A : integer
-    # @param B : integer
-    # @return a strings
+	# @param A : integer
+	# @param B : integer
+	# @return a strings
     def fractionToDecimal(self, A, B):
-            
-        retlist = []
-        if (A < 0) ^ (B < 0) and (A != 0):
-            retlist.append('-')
-        A = abs(A)
-        B = abs(B)
-        retlist.append(str(A//B))
-        if A%B:
-            retlist.append('.')
-        hashmap = {}
-        # 44/30 ---- 1 . 14%30 - 14 
-        rem = A % B
-        while  rem and  rem not in hashmap:
-            hashmap[rem] = len(retlist) - 1
-            rem = 10 * rem 
-            retlist.append(str(rem // B))
-            rem = rem % B
-        if rem:
-            retlist.insert(hashmap[rem]+1, '(')
-            retlist.append(')')
-        return ''.join(val for val in retlist)
+        if A % B == 0: return str(A//B)
+        neg = False
+        if (A<0 or B<0) and not (A<0 and B<0): 
+            neg = True
+            A, B = abs(A), abs(B)
+        intstr = str(A//B)
+        modularDict = defaultdict(bool)
+        modularIdx, decimalIdx = {}, 0
+        modular = A%B
+        decimals = []
+        while modularDict[modular] == False and modular != 0:
+            modularDict[modular] = True
+            modularIdx[modular] = decimalIdx
+            decimalIdx += 1
+            modular *= 10
+            decimals.append(str(modular//B))
+            modular = modular % B
+        if modularDict[modular]:
+            #divide into before and in the brackets
+            beforeBra = "".join(decimals[:modularIdx[modular]])
+            inBra = "("+"".join(decimals[modularIdx[modular]:])+")"
+            decimals = [beforeBra, inBra]
+        if neg:
+            return "-"+intstr+"."+"".join(decimals)
+        return intstr+"."+"".join(decimals)

fd694ef23858f21d31250dd089520535d82ceb7e
Woody Yao <bogikidd@gmail.com>
2020-01-30 15:37:15 +0200

    Update Points_on_the_Straight_Line.py

--- Hashing/Maths and hashing/Points_on_the_Straight_Line.py
+++ Hashing/Maths and hashing/Points_on_the_Straight_Line.py
@@ -1,36 +1,35 @@
-'''
-definition: points on the same straight line
-means: (x2 - x1) / (y2 - y1) are the same
-=> (x2 - x1) / (y2 - y1) - (x3 - x1) / (y3 - y1) = 0
-=> (x2 - x1)(y3 - y1) - (x3 - x1)(y2 - y1) = 0
-'''
-
 from collections import defaultdict
 class Solution:
     # @param A : list of integers
     # @param B : list of integers
     # @return an integer
     def maxPoints(self, A, B):
+        """
+        on the same line => got the same slope
+        y = ax + b, so points on the same line means
+        they need to have the same slope(a) and same interval(b)
+        """
         n = len(A)
-        if (n<=2):
-            return n
-        m = 0 #result value
-        for i in range(n):
-            l = {}  #every time reset the dict
-            dup = 1 #count the duplicates
+        output = 0
+        if n <= 2: return n
+        for i in range(n-1):
+            slopeDict = defaultdict(int)
+            dup = 0
             for j in range(n):
-                if (A[i]==A[j] and B[i]==B[j] and i!=j):        
-                    dup+=1  #count duplicates
-                elif i!=j :
-                    if (A[i]==A[j]):  #vertical line
-                        l['v'] = l.get('v',0) + 1
-                    elif (B[i]==B[j]): # horizontal line
-                        l['h'] = l.get('h',0) + 1
-                    else:   #regular slope
-                        slope = 1.0*(B[i]-B[j])/(A[i]-A[j])
-                        l[slope] = l.get(slope,0)+1
-            if (len(l)>0): 
-                m = max(m,max(l.values())+dup)
-            else: #if all points are duplicates, l is empty.
-                m = max(m,dup)
-        return m
+                if A[i] == A[j] and B[i] == B[j]:
+                    dup += 1
+                    continue
+                elif A[i] == A[j]:
+                    slope = float("inf")
+                elif B[i] == B[j]:
+                    slope = 0
+                else:
+                    slope = (float(B[i] - B[j])/float(A[i] - A[j]))
+                slopeDict[slope] += 1
+            if slopeDict:
+                temp = max(slopeDict.values())
+            else: temp = 0
+            temp += dup
+            if temp > output:
+                output = temp
+        return output

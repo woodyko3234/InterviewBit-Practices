913cf3cbcd9d08121b03e4a736a8950b1b7def09
Woody Yao <bogikidd@gmail.com>
2020-05-01 15:14:40 -0400

    Create Identical_Binary_Trees.py

---
+++ Tree Data Structure/2 trees/Identical_Binary_Trees.py
@@ -0,0 +1,43 @@
+# Definition for a  binary tree node
+# class TreeNode:
+#    def __init__(self, x):
+#        self.val = x
+#        self.left = None
+#        self.right = None
+
+class Solution:
+    # @param root_A : root node of tree
+    # @param root_B : root node of tree
+    # @return an integer
+    def isSameTree(self, root_A, root_B):
+        if root_A == None and root_B == None:
+            pass
+        elif root_A == None or root_B == None: 
+            #print("notsametree")
+            return 0
+        elif not self.isBranchSame(root_A, root_B): 
+            #print("notsamebranch")
+            return 0
+        elif not self.isSameNode(root_A, root_B):
+            #print("notsamenode")
+            return 0
+        else:
+            if self.isSameTree(root_A.left, root_B.left) == 0 or self.isSameTree(
+                root_A.right, root_B.right) == 0: return 0
+        return 1
+        
+    def isSameNode(self, node_A, node_B):
+        if node_A.val != node_B.val: return 0
+        return 1
+    
+    def isLeaf(self, d):
+        if d.left is None and d.right is None:
+            return 1
+        return 0
+    
+    def isBranchSame(self, node_A, node_B):
+        if (node_A.left and not node_B.left) or (not node_A.left and node_B.left):
+            return 0
+        elif (node_A.right and not node_B.right) or (not node_A.right and node_B.right):
+            return 0
+        return 1

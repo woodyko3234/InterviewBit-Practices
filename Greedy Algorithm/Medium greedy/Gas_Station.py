dc66ed4ef3f17adea594140e338cd990d2f1e796
Woody Yao <bogikidd@gmail.com>
2020-09-29 16:32:46 +0800

    Update Gas_Station.py

--- Greedy Algorithm/Medium greedy/Gas_Station.py
+++ Greedy Algorithm/Medium greedy/Gas_Station.py
@@ -1,22 +1,26 @@
 class Solution:
-    # @param A : tuple of integers, filling gas
-    # @param B : tuple of integers, consuming gas
+    # @param gas : tuple of integers
+    # @param cost : tuple of integers
     # @return an integer
-    def canCompleteCircuit(self, A, B):
-        #qualified starting points
-        qS, C = [], []
-        n = len(A)
-        for i in range(n):
-            C.append(A[i] - B[i])
-            if A[i] - B[i] >= 0:
-                qS.append(i)
-        if sum(C) < 0: return -1 #definitely cannot finish
-        for i in qS:
-            route = C[i]
-            j = (i+1) % n
-            while j != i:
-                route += C[j]
-                if route < 0: break
-                j += 1
-            if route >= 0: return i
-        return -1
+    def canCompleteCircuit(self, gas, cost):
+        '''The bruteforce solution should be obvious. Start from every i, 
+        and check to see if every point is reachable with the gas available. 
+        Return the first i for which you can complete the trip without the gas reaching 
+        a negative number. 
+        This approach would however be quadratic.
+        Lets look at how we can improve. 
+        
+        1) If sum of gas is more than sum of cost, 
+        does it imply that there always is a solution ? 
+        2) Lets say you start at i, and hit first negative of 
+        sum(gas) - sum(cost) at j. We know TotalSum(gas) - TotalSum(cost) > 0. 
+        What happens if you start at j + 1 instead ? 
+        Does it cover the validity clause for i to j already ?'''
+        gas_left = gas_needed = start = 0
+        for i, (g, c) in enumerate(zip(gas, cost)):
+            gas_left += g - c
+            if gas_left < 0:
+                gas_needed -= gas_left
+                start = i + 1
+                gas_left = 0
+        return start if gas_left >= gas_needed else -1
